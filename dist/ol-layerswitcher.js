63f6633eba485746ef52beaa2d0a0a772bb653c8
Stephen Woodbridge <woodbri@swoodbridge.com>
2019-02-02 09:39:07 -0500

    Debugging support ajax requests to support building controls.

--- dist/ol-layerswitcher.js
+++ dist/ol-layerswitcher.js
@@ -449,8 +449,9 @@ var LayerSwitcher = function (_Control) {
                     }).done(function () {
                         alert('done');
                         LayerSwitcher.buildControlCallback_(lyr, ul, label, sel, data, callback);
-                    }).fail(function () {
-                        alert('fail');
+                    }).fail(function (jqHXR, textStatus, errorThrown) {
+                        console.log(textStatus);
+                        alert('fail:' + textStatus);
                     }).always(function () {
                         alert('complete');
                     });
--- src/ol-layerswitcher.js
+++ src/ol-layerswitcher.js
@@ -244,8 +244,9 @@ export default class LayerSwitcher extends Control {
                     alert('done');
                     LayerSwitcher.buildControlCallback_(lyr, ul, label, sel, data, callback);
                 })
-                .fail(function() {
-                    alert('fail');
+                .fail(function(jqHXR, textStatus, errorThrown) {
+                    console.log(textStatus);
+                    alert('fail:' + textStatus);
                 })
                 .always(function() {
                     alert('complete');
